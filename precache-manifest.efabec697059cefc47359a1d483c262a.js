49ed97bd268e7a11222627eae9e6c46ad6072337
tlebon <tlebon@gmail.com>
2018-10-28 15:53:18 +0100

    Updates

---
+++ precache-manifest.efabec697059cefc47359a1d483c262a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b812b300db6f2809d28585868d1e6c82",
+    "url": "/neo-hackathon/static/media/plane-info.b812b300.png"
+  },
+  {
+    "revision": "9618e0bbcc5513cab694dfb0f45ca156",
+    "url": "/neo-hackathon/static/media/gear.9618e0bb.png"
+  },
+  {
+    "revision": "73b7edf70cb51508de70d8eab7dd6789",
+    "url": "/neo-hackathon/static/media/Logo.73b7edf7.svg"
+  },
+  {
+    "revision": "fea0122961de239103f2",
+    "url": "/neo-hackathon/static/js/runtime~main.fea01229.js"
+  },
+  {
+    "revision": "aa0fef8fab892e6b7652",
+    "url": "/neo-hackathon/static/js/main.aa0fef8f.chunk.js"
+  },
+  {
+    "revision": "d879dff8749ed46557bc",
+    "url": "/neo-hackathon/static/js/1.d879dff8.chunk.js"
+  },
+  {
+    "revision": "aa0fef8fab892e6b7652",
+    "url": "/neo-hackathon/static/css/main.2dfe334f.chunk.css"
+  },
+  {
+    "revision": "ad4f959ea5b46af928fbc284adb5c86a",
+    "url": "/neo-hackathon/index.html"
+  }
+];
